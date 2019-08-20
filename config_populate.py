ad3ddcd5e94f6d568843f077285ed0fac78ecde7
hapax.io <39811582+thehapax@users.noreply.github.com>
2019-08-20 10:06:27 -0700

    refactor

--- config_populate.py
+++ config_populate.py
@@ -1,5 +1,14 @@
 import configparser
 
+"""
+an idea of future config file format
+Simple Arb : A <-> B
+Triangular arb : A -> B -> C
+"""
+simple_arb = ['strategy-type', 'exchange_name', 'api_key', 'secret']
+triangular_arb = ['strategy-type', 'Exchange_Name_A', 'api_key_A', 'secret-A',
+                  'Exchange_Name_C', 'api_key_C', 'secret-C']
+
 def add_section(strategy_name, strategy, parser):
     parser.add_section(strategy_name)
     for label in strategy:
@@ -7,24 +16,15 @@ def add_section(strategy_name, strategy, parser):
         parser.set(strategy_name, label, value)
     return parser
 
-
-# an idea of future format?
-simple_arb = ['exchange-name', 'api-key', 'secret']
-triangular_arb = ['Exchange_Name_A', 'api_key_A', 'secret-A',
-                  'Exchange_Name_C', 'api_key_C', 'secret-C']
-
-# Triangular arb : A -> B -> C
-
 # get data from user, write config to file
 parser = configparser.ConfigParser()
 
-my_strategy = input("Name of simple-arb strategy:")
+my_strategy = input("Name of strategy:")
 add_section(my_strategy, simple_arb, parser)
 
-tristrategy = input("Name of Triangular Strategy:")
+tristrategy = input("Name of strategy:")
 add_section(tristrategy, triangular_arb, parser)
 
-
 # write parser to file
-with open('test.ini', 'w') as writer:
+with open('safe/secrets_test2.ini', 'w') as writer:
     parser.write(writer)
