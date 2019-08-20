2b7d142c8a86273fa72740af21adc1ade9a62e10
hapax.io <39811582+thehapax@users.noreply.github.com>
2019-08-19 20:55:44 -0700

    refactor

--- config_populate.py
+++ config_populate.py
@@ -1,48 +1,28 @@
 import configparser
 
-# idea of future format?
-simple_arb = ['strategy-name', 'exchange-name', 'api-key', 'secret']
-triangular_arb = ['triangular-strategy', 'A-exchange-name', 'A-api-key', 'A-secret',
-                  'C-exchange-name', 'C-api-key', 'C-secret']
+def add_section(strategy_name, strategy, parser):
+    parser.add_section(strategy_name)
+    for label in strategy:
+        value = input(label + ":")
+        parser.set(strategy_name, label, value)
+    return parser
+
+
+# an idea of future format?
+simple_arb = ['exchange-name', 'api-key', 'secret']
+triangular_arb = ['Exchange_Name_A', 'api_key_A', 'secret-A',
+                  'Exchange_Name_C', 'api_key_C', 'secret-C']
 
 # Triangular arb : A -> B -> C
-####################################
+
 # get data from user, write config to file
-
-strategy = 'simple-arb-strategy'
-exchange_label = 'exchange_name'
-api_key_label = 'api_key'
-secret_label = 'secret'
-
 parser = configparser.ConfigParser()
 
-# default
-exchange = 'cointiger'
-api_key = 'testf136123409824037840f0c160'
-secret = 'secret_here'
+my_strategy = input("Name of simple-arb strategy:")
+add_section(my_strategy, simple_arb, parser)
 
-parser.add_section(strategy)
-parser.set(strategy, exchange_label, exchange)
-parser.set(strategy, api_key_label, api_key)
-parser.set(strategy, secret_label, secret)
-
-tristrategy = input("Name of Strategy:")
-exchangeA = input("exchange A: ")
-api_keyA = input("api key A:")
-secretA = input("secret A:")
-exchangeC = input("exchange C: ")
-api_keyC = input("api key C:")
-secretC = input("secret C:")
-
-parser.add_section(tristrategy)
-
-parser.set(tristrategy, triangular_arb[1], exchangeA)
-parser.set(tristrategy, triangular_arb[2], api_keyA)
-parser.set(tristrategy, triangular_arb[3], secretA)
-
-parser.set(tristrategy, triangular_arb[4], exchangeC)
-parser.set(tristrategy, triangular_arb[5], api_keyC)
-parser.set(tristrategy, triangular_arb[6], secretC)
+tristrategy = input("Name of Triangular Strategy:")
+add_section(tristrategy, triangular_arb, parser)
 
 
 # write parser to file
