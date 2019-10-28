71603a4823dde57cc80693cc9ddb4580e0ac0c3d
thehapax <39811582+thehapax@users.noreply.github.com>
2019-10-28 13:23:51 -0700

    clean up test methods

--- experiments/ccxt_exchange_test.py
+++ experiments/ccxt_exchange_test.py
@@ -5,8 +5,8 @@ import ccxt
 import json
 from ccxt_exchange import CcxtExchange
 import time
+import pandas as pd
 
-#from ccxt_engine import CcxtOrderEngine
 """
     Temporary informal unit test for ccxt exchange
 """
@@ -26,7 +26,6 @@ def get_exchange_config():
         parser = configparser.ConfigParser()
         parser.read(os.path.join(config_dir, config_file))
         exch_ids = parser.sections()
-        log.info(exch_ids)
         sec = {section_name: dict(parser.items(section_name)) for section_name in exch_ids}
         return sec
     except Exception as e:
@@ -41,7 +40,6 @@ def get_exchange(config_sections):
     apikey = config_sections[exch_name]['api_key']
     secret = config_sections[exch_name]['secret']
     log.info(f"API Key:  {apikey}")
-    log.info(f"SECRET: {secret})")
 
     # coin tiger requires an API key, even if only for ticker data
     ccxt_ex = getattr(ccxt, exch_name)({
@@ -56,14 +54,8 @@ def get_exchange(config_sections):
 
 def get_ccxt_module():
     config_sections = get_exchange_config()
-    log.info(config_sections)
     ccxt_ex = get_exchange(config_sections)
     return ccxt_ex
-
-
-def get_test_l2ob(symbol, ccxt_ex):
-    l2_ob = cx.fetch_l2_order_book(symbol)
-    return l2_ob
 
 
 def write_dict(l2_ob, file_name):
@@ -77,73 +69,102 @@ def read_dict(file_name):
     return static_ob
 
 
-def test_rw_ob(l2_ob):
+def test_rw_ob(l2_ob, file_name):
+    """
+    Test Reading and writing orderbook to file
+    :param l2_ob:
+    :return:
+    """
     # write order book to file.
     # read order book from file
-    file_name = 'cex_ob.txt'
+    if file_name is None:
+        file_name = 'cex_ob.txt'
     write_dict(l2_ob, file_name)
     static_ob = read_dict(file_name)
-    print(static_ob)
+    log.info(static_ob)
+
+
+def test_print_orderbooks(symbol, cx):
+    log.info(f"Fetch Ticker for {symbol} : {ccxt_ex.fetch_ticker(symbol)}\n")
+    log.info(f"Fetching L2 Order Book: {cx.fetch_l2_order_book(symbol)}\n")
+    log.info(f"Fetching Order Book: {cx.fetch_order_book(symbol)}\n")
+
+
+def get_cex_data(l2, depth: int):
+    # let ob stand for orderbook, ob_depth is the order book depth we want to map out
+
+    bids = l2['bids']
+    bid_df = pd.DataFrame(bids)
+    bid_df.columns = ['price', 'vol']
+    bid_df['timestamp'] = l2['timestamp']
+    bid_df['type'] = 'bids'
+
+    ask = l2['asks']
+    ask_df = pd.DataFrame(ask)
+    ask_df.columns = ['price', 'vol']
+    ask_df['timestamp'] = l2['timestamp']
+    ask_df['type'] = 'asks'
+
+    return ask_df.head(depth), bid_df.head(depth)
 
 
 
 if __name__ == '__main__':
 
-    symbol = 'BTC/USDT'
-    #    symbol = 'BTS/BTC'
+    #symbol = 'BTC/USDT'
+    symbol = 'BTS/BTC'
+    symbol = 'BTS/ETH'
 
     config_sections = get_exchange_config()
     log.info(config_sections)
     ccxt_ex = get_exchange(config_sections)
     cx = CcxtExchange(exchange=ccxt_ex)
-    
-    log.info(f"Available Free Balance: {cx.free_balance}\n")
-    
-    log.info(f"Fetch Ticker for {symbol} : {ccxt_ex.fetch_ticker(symbol)}\n")
-    l2_ob = get_test_l2ob(symbol, ccxt_ex)
-    l2_ob = cx.fetch_order_book(symbol)
 
-    print("Free Balance:")
+    log.info(f'symbol: {symbol}')
+    #test_print_orderbooks(symbol, cx)
+
+    l2_ob = ccxt_ex.fetch_l2_order_book(symbol)
+    asks, bids = get_cex_data(l2_ob, depth=2)
+    log.info(f'Asks:\n {asks}')
+    log.info(f'Bids:\n {bids}')
+
+    log.info("CCXT method list ")
+    method_list = cx.method_list
+    log.info(f"Available Methods from ccxt for this exchange: {list(method_list)}")
+
+    log.info(f"Available Free Balance: {cx.free_balance}\n")
     free_bal = cx.free_balance
     bts_free = free_bal['BTS']
-    print(f"BTS : {bts_free}")
+    log.info(f"BTS : {bts_free}")
 
     log.info(f"Fetch my trades {symbol}: Trades: {ccxt_ex.fetch_my_trades(symbol)}\n")
 
-    print("Fetch Open Orders")
+    log.info("Fetch Open Orders")
     open_orders = ccxt_ex.fetch_open_orders(symbol=symbol)
 
-    print("ccxt method list ")
-    method_list = cx.method_list
-    log.info(f"Available Methods from ccxt for this exchange {method_list}")
-    
     """
     test buy and sell on cex exchanges
     """
-
-#   symbol = 'BTS'
     buy_amt = 0.0001
     buy_price = 1000000
     since = time.time()-10000
     
-    #   print("Fetch trading fees") # may not exist for some exchanges, check method_list
-    #   print(cx.fetch_trading_fees())
-
+    # may not exist for some exchanges, check method_list
+    fees = cx.fetch_trading_fees()
+    log.info(f'Fetch Trading Fees: {fees}')
     
-    # ccxt_ex.create_buy_order(symbol, buy_amt, buy_price)
-    print("fetch my trades:")
-    print(cx.fetch_my_trades(symbol))
-
-    print("fetch open orders:")
-    print(cx.fetch_open_orders(symbol))
-
-    print("fetch closed orders:")
-    print(cx.fetch_closed_orders(symbol, since))
-
+    # cx.create_buy_order(symbol, buy_amt, buy_price)
     # cx.fetch_order(order_id)
-    # cx.cancel_order(order_id)
-    # cx.create_sell_order(symbol, sell_amt, sell_price)
 
+    log.info(f'fetch my trades: {cx.fetch_my_trades(symbol)}')
+    log.info(f'fetch open orders: {cx.fetch_open_orders(symbol)}')
+    log.info(f'fetch closed orders: {cx.fetch_closed_orders(symbol, since)}')
+
+    # cx.create_sell_order(symbol, sell_amt, sell_price)
+    # cx.create_buy_order(symbol, sell_amt, sell_price)
+
+    # cx.cancel_order(order_id)
+    
     # todo:
     #    def get_all_closed_orders_since_to(self, symbol, since, to):
 
@@ -151,7 +172,6 @@ if __name__ == '__main__':
     
 """
     cx = CcxtExchange(exchange=ccxt_ex)
-    trade_executor = CcxtOrderEngine(cx)
 
     log.info(f"Available Methods from ccxt for this exchange {list(cx.method_list)}\n")
     log.info(f"Available Free Balance: {cx.free_balance}\n")
